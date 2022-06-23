efaf552d2f511e83851db575f8accd6bc536eae2
tarex7 <tarex7@gmail.com>
2022-06-23 15:09:04 +0200

    FIX logica multipli

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 for (let i = 1; i <= 100; i++) {
-  console.log(i);
-  if(i % 3 === 0) {
-    console.log(i, `FIZZ`);
-  }
-  if(i % 5 === 0) {
-    console.log(i , `BUZZ`);
-  }
-  if(i % 5 === 0 && i % 3 === 0) {
+  if (i % 3 === 0 && i % 5 !== 0) {
+    console.log(i, "FIZZ");
+  } else if (i % 5 === 0 && i % 3 !== 0) {
+    console.log(i, "BUZZ");
+  } else if (i % 3 === 0 && i % 5 === 0) {
     console.log(i, "FIZZBUZZ");
+  } else {
+    console.log(i);
   }
 }
